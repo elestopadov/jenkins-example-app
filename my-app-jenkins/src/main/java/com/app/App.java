298ba2b747bd9a1e523f58b88a8150c9c29298ec
Gandur <ganduras_@mail.ru>
2022-05-17 15:48:53 +0400

    added project

---
+++ my-app-jenkins/src/main/java/com/app/App.java
@@ -0,0 +1,15 @@
+package com.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a = 0;
+        String ne_w;
+        System.out.println( "Hello World from Jenkins!" );
+    }
+}
---
+++ web-app/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Web application</h2>
+<h3>Continuous integration with jenkins</h3>
+</body>
+</html>
